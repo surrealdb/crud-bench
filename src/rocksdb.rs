8d9ddc9c5e5e22133883134784934940c4a78d3b
Tobie Morgan Hitchcock <tobie@surrealdb.com>
2024-12-29 18:18:30 +0000

    Add block cache to RocksDB and SpeeDB (#55)

--- src/rocksdb.rs
+++ src/rocksdb.rs
@@ -6,9 +6,10 @@ use crate::valueprovider::Columns;
 use crate::{KeyType, Projection, Scan};
 use anyhow::{bail, Result};
 use rocksdb::{
-	BottommostLevelCompaction, CompactOptions, DBCompactionStyle, DBCompressionType, FlushOptions,
-	IteratorMode, LogLevel, OptimisticTransactionDB, OptimisticTransactionOptions, Options,
-	ReadOptions, Transaction, WaitForCompactOptions, WriteOptions,
+	BlockBasedOptions, BottommostLevelCompaction, Cache, CompactOptions, DBCompactionStyle,
+	DBCompressionType, FlushOptions, IteratorMode, LogLevel, OptimisticTransactionDB,
+	OptimisticTransactionOptions, Options, ReadOptions, Transaction, WaitForCompactOptions,
+	WriteOptions,
 };
 use serde_json::Value;
 use std::hint::black_box;
@@ -60,6 +61,12 @@ impl BenchmarkEngine<RocksDBClient> for RocksDBClientProvider {
 			DBCompressionType::Snappy,
 			DBCompressionType::Snappy,
 		]);
+		// Set the block cache size in bytes
+		let mut block_opts = BlockBasedOptions::default();
+		let cache = Cache::new_lru_cache(256 * 1024 * 1024);
+		block_opts.set_bloom_filter(10.0, false);
+		block_opts.set_block_cache(&cache);
+		opts.set_block_based_table_factory(&block_opts);
 		// Create the store
 		Ok(Self(Arc::new(OptimisticTransactionDB::open(&opts, DATABASE_DIR)?)))
 	}
