e82c58df71b0be05a282d23cdafd870e57d9a3d8
Tobie Morgan Hitchcock <tobie@surrealdb.com>
2025-01-12 20:32:35 +0000

    Ensure `ulimit` is applied to Neo4j Docker container

--- src/neo4j.rs
+++ src/neo4j.rs
@@ -16,7 +16,7 @@ use std::hint::black_box;
 
 pub(crate) const NEO4J_DOCKER_PARAMS: DockerParams = DockerParams {
 	image: "neo4j",
-	pre_args: "-p 127.0.0.1:7474:7474 -p 127.0.0.1:7687:7687 -e NEO4J_AUTH=none",
+	pre_args: "--ulimit nofile=65536:65536 -p 127.0.0.1:7474:7474 -p 127.0.0.1:7687:7687 -e NEO4J_AUTH=none",
 	post_args: "",
 };
 
